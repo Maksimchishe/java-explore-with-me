2c4a2394717f2c833760b27b405dacc5d60f327b
Max <89996069795max@gmail.com>
2025-05-14 21:25:58 +0000

    Этап 2. Основной сервис.

--- main-service/src/main/java/ru/practicum/ewm/service/event/EventServiceImpl.java
+++ main-service/src/main/java/ru/practicum/ewm/service/event/EventServiceImpl.java
@@ -64,7 +64,7 @@ public class EventServiceImpl implements EventService {
         if (newEventDto.getAnnotation().isBlank()) {
             throw new ValidationException("Поле Annotation не может быть пустым.");
         }
-        if (newEventDto.getParticipantLimit() < 0) {
+        if (newEventDto.getParticipantLimit() < 0L) {
             throw new ValidationException("Поле ParticipantLimit не может быть отрицательным.");
         }
         if (newEventDto.getEventDate().minusHours(2).isBefore(LocalDateTime.now())) {
@@ -93,9 +93,7 @@ public class EventServiceImpl implements EventService {
     public EventFullDto updateEventByInitiator(Long userId, Long eventId, UpdateEventUserRequest updateEventUserRequest) {
         Event eventToUpdate = eventRepository.findById(eventId)
                 .orElseThrow(() -> new NotFoundException("Event does not exist " + eventId));
-        if (updateEventUserRequest.getParticipantLimit() < 0) {
-            throw new ValidationException("Поле ParticipantLimit не может быть отрицательным.");
-        }
+
         if (eventToUpdate.getState().equals(EventState.CANCELED) || eventToUpdate.getState().equals(EventState.PENDING)) {
             if (updateEventUserRequest.getEventDate() != null
                 && updateEventUserRequest.getEventDate().isBefore(LocalDateTime.now().plusHours(2))) {
